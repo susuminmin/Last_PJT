c35c48b7654e9cdc94f5a06a79cdd06657142e75
김수민 <mytoutbon@gmail.com>
2019-11-22 15:17:01 +0900

    Make database-algorithm
     - 영화진흥위원회 API와 네이버 영화 검색 API 를 통합하여 하나의 csv 파일
     작성
     - BeautifulSoup 모듈을 활용하여 하이퍼텍스트 링크에서 영화 줄거리를
     셀렉트한 후 csv 파일에 추가함

--- movie_list.py
+++ movie_list.py
@@ -4,16 +4,47 @@ from pprint import pprint
 from datetime import datetime, timedelta
 from decouple import config
 
+# BASE_URL = 'http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchWeeklyBoxOfficeList.json'
+# key = config('API_KEY')
+# weekGb = '0'
+
+# with open('movie.csv', 'w', encoding='utf-8') as f:
+#     fieldnames = ['기간', '영화코드', '영화제목', '순위']
+#     writer = csv.DictWriter(f, fieldnames=fieldnames)
+#     writer.writeheader()
+
+#     for i in range(900):
+#         targetDt = datetime(2019, 11, 17) - timedelta(weeks=i)
+#         targetDt = targetDt.strftime('%Y%m%d')
+#         API_URL = f'{BASE_URL}?key={key}&targetDt={targetDt}&weekGb=0'
+
+#         response = requests.get(API_URL)
+#         data = response.json()
+#         movie_data = {}
+
+#         showRange = data['boxOfficeResult']['showRange']
+
+#         for movie in data['boxOfficeResult']['weeklyBoxOfficeList']:
+#             movie_data[movie.get('movieCd')] = {
+#                 '기간': showRange ,
+#                 '영화코드': movie.get('movieCd'),
+#                 '영화제목': movie.get('movieNm'),
+#                 '순위': movie.get('rank'),
+#             }
+
+#         for item in movie_data.values():
+#             writer.writerow(item)
+
 BASE_URL = 'http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchWeeklyBoxOfficeList.json'
 key = config('API_KEY')
 weekGb = '0'
 
 with open('movie.csv', 'w', encoding='utf-8') as f:
-    fieldnames = ['기간', '영화코드', '영화제목', '순위']
+    fieldnames = ['기간순위', '기간', '기간시작', '기간종료', '영화코드', '영화제목', '순위']
     writer = csv.DictWriter(f, fieldnames=fieldnames)
     writer.writeheader()
 
-    for i in range(900):
+    for i in range(5):
         targetDt = datetime(2019, 11, 17) - timedelta(weeks=i)
         targetDt = targetDt.strftime('%Y%m%d')
         API_URL = f'{BASE_URL}?key={key}&targetDt={targetDt}&weekGb=0'
@@ -23,10 +54,17 @@ with open('movie.csv', 'w', encoding='utf-8') as f:
         movie_data = {}
 
         showRange = data['boxOfficeResult']['showRange']
+        start = showRange[:8]
+        end = showRange[9:]
+        # print(start)
+        # print(end)
 
         for movie in data['boxOfficeResult']['weeklyBoxOfficeList']:
             movie_data[movie.get('movieCd')] = {
-                '기간': showRange ,
+                '기간순위': showRange + movie.get('rank'),
+                '기간': showRange,
+                '기간시작': start,
+                '기간종료': end,
                 '영화코드': movie.get('movieCd'),
                 '영화제목': movie.get('movieNm'),
                 '순위': movie.get('rank'),
--- naver_movie.py
+++ naver_movie.py
@@ -31,17 +31,17 @@ for movie_title, movie_code in query_dict.items():
     if response.get('items')[0].get('link') == '':
         continue
     movie_code.update(영화_썸네일_이미지의_URL=response.get('items')[0].get('image'))
-    hytext = response.get('items')[0].get('link')
-    # print('1,', hytext)  /  link만 따옴
+    link = response.get('items')[0].get('link')
+    # print('1,', link)  /  link만 따옴
 
-    hyp_link = requests.get(hytext)
+    hyp_link = requests.get(link)
     # print(hyp_link.status_code)  /  200 -> 요청이 제대로 가짐
     # print('2', hyp_link)  /  200 -> 요청이 제대로 가짐
     html = hyp_link.text  # 응답받은 객체에서 html문서를 string으로 바꾸는 것
     soup = bs4.BeautifulSoup(html, 'html.parser')
     # print(soup)
     discription = soup.select('div.story_area p.con_tx')
-    print(discription)
+    # print(discription)
 
 
 
