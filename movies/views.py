c5770d01d38835ad3e80d9e9f491e4dd9a022e18
조성원 <sungwoncho94@naver.com>
2019-11-25 15:23:03 +0900

    base.html sidebar 배경 색상 변경

--- movies/views.py
+++ movies/views.py
@@ -9,13 +9,18 @@ from .forms import ReviewForm, SearchedDateForm
 # 로그인이 되었다고 가정하고 해보자
 @require_GET
 def index(request):
-    # 사이드바에서 제공할 데이터들
-    user = request.user
-    # 사용자가 클릭한 영화들 모두 가져오기  //  사용자가 검색한 날짜들 모두 가져오기
-    clicked_movies = user.clicked_movies.all()
-    searched_dates = user.searched_dates.all()
-    context = {
+    if request.user.is_authenticated:
+        # 사이드바에서 제공할 데이터들
+        user = request.user
+        # 사용자가 클릭한 영화들 모두 가져오기  //  사용자가 검색한 날짜들 모두 가져오기
+        clicked_movies = user.clicked_movies.all()
+        searched_dates = user.searched_dates.all()
+        context = {
         'clicked_movies': clicked_movies,
         'searched_dates': searched_dates
-    }
-    return render(request, 'movies/index.html', context)
+        }
+        return render(request, 'movies/index.html', context)
+    else:
+        return render(request, 'movies/index.html')
+    
+    
