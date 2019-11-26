a6f782d168974190296f033d16512fdfc7575309
mytoutbon <mytoutbon@gmail.com>
2019-11-26 14:47:24 +0900

    movie_naver.py 의 정보를 읽어와 DB로 저장하는 로직을 담은 test.py 파일 작성

--- movies/models.py
+++ movies/models.py
@@ -3,23 +3,15 @@ from django.conf import settings
 
 
 # MakeDB모델을 거쳐서 movie.csv -> 네이버API -> (Django MakeDB모델) -> DB
-class MakeDB(models.Model):
-    Range_rank = models.CharField(max_length=30)
+class Movie(models.Model):
+    # 필요한 정보 : rank, 국문 title, 영문 title, description, poster_url, naver_movie_url, -> csv에서부터 db를 만들기 위해 필요한 필드
+    range_rank = models.CharField(max_length=30)
     showRange = models.CharField(max_length=30)
-    start = models.CharField(max_length=10)
-    end = models.CharField(max_length=10)
     poster_url = models.CharField(max_length=200)
     title = models.CharField(max_length=20)
     movie_code = models.IntegerField()
     naver_movie_url = models.CharField(max_length=200)
     description = models.TextField()
-
-
-class Movie(models.Model):
-    # 필요한 정보 : rank, 국문 title, 영문 title, description, poster_url, naver_movie_url, -> csv에서부터 db를 만들기 위해 필요한 필드
-    title = models.CharField(max_length=30)
-    description = models.TextField()
-    poster_url = models.CharField(max_length=200)
     clicked_users = models.ManyToManyField(  # movie.clicked_users.all()  =>  해당 영화를 클릭한 모든 유저를 불러오는 방법
         settings.AUTH_USER_MODEL,
         # user.clicked_movies.all()  =>  해당 유저가 클릭만 모든 영화를 불러오는 방법
