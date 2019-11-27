47375e213bb0a98d6278953f789cd2394efce402
조성원 <sungwoncho94@naver.com>
2019-11-27 20:46:41 +0900

    배경화면 삽입, movie_list & movie_review 페이지 css변경

--- movies/forms.py
+++ movies/forms.py
@@ -4,6 +4,14 @@ from .models import Review, SearchedDate
 
 class ReviewForm(forms.ModelForm):
 
+    def __init__(self, *args, **kwargs):
+        super(ReviewForm, self).__init__(*args, **kwargs)
+        self.fields['score'].widget.attrs['max'] = 5
+        self.fields['score'].widget.attrs['min'] = 1
+        self.fields['content'].widget.attrs['placeholder'] = '댓글을 입력하세요.'
+        self.fields['content'].widget.attrs['size'] = 60
+        self.fields['score'].widget.attrs['size'] = 10
+
     class Meta:
         model = Review
         fields = ['content', 'score', ]
