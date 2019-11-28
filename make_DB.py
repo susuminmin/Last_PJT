aca79edb36dec124a80256a1bd5575293538a45c
sungwoncho94 <sungwoncho94@naver.com>
2019-11-28 14:48:04 +0900

    DB에 영화 타이틀과 배우 목록 추가, 이에 따른 모델 수정

--- make_DB.py
+++ make_DB.py
@@ -7,28 +7,22 @@ from bs4 import BeautifulSoup
 import os
 import django
 from django.db import transaction
-
-
 # django setting 파일 설정하기 및 장고 셋업
 cur_dir = os.path.dirname(__file__)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lastpjt.settings")
 django.setup()
 # 모델 임포트는 django setup이 끝난 후에 가능하다. 셋업 전에 import하면 에러난다. db connection 정보가 없어서......
 from movies.models import Movie
-
 # 네이버에서 영화 줄거리 가져오는 주소
 BASE_URL = 'https://openapi.naver.com/v1/search/movie.json'
 # 네이버에서 영화 포스터 가져오는 주소
 BASE_IMAGE_URL = 'https://movie.naver.com/movie/bi/mi/photoViewPopup.nhn?movieCode='
-
-
 clientId = config('CLIENT_ID')
 clientSecret = config('CLIENT_SECRET')
 HEADERS = {
     'X-Naver-Client-Id': clientId,
     'X-Naver-Client-Secret': clientSecret,
 }
-
 query_dict = {} 
 # 빈 qurey_dict 에 XXXXX (영화제목 값) 을 key로, {'영화코드': XXXXXXXX} 를 value 로 하는 자료 추가할 것 
 with open('movie.csv', 'r', newline='', encoding='utf-8') as f:
@@ -43,27 +37,26 @@ with open('movie.csv', 'r', newline='', encoding='utf-8') as f:
             '영화명(국문)': row['영화명(국문)'],
             '기간순위': row['기간순위'],
             }
-
 # 네이버에서 영화에 맞는 줄거리와 포스터url를 받아와서 dict에 합친 후,
 # model.py의 MakeDB모델을 거쳐 우리 DB에 저장한다
-fieldnames = ('순위', '기간시작', '기간종료', '썸네일_이미지의_URL', '영화명(국문)', '영화코드', '하이퍼텍스트_링크', '줄거리')
+fieldnames = ('순위', '기간시작', '기간종료', '썸네일_이미지의_URL', '영화명(국문)', '영화명(영문)', '배우', '영화코드', '하이퍼텍스트_링크', '줄거리')
 with open('movie_naver.csv', 'w', encoding='utf-8', newline='') as f:
     writer = csv.DictWriter(f, fieldnames=fieldnames, extrasaction='ignore')
     writer.writeheader()
-
     for week_rank, movie_dict in query_dict.items(): # key 값에 대한 for문
         movie_title = movie_dict['영화명(국문)']
-
-        API_URL = f'{BASE_URL}?query={movie_title}'
+        API_URL = f'{BASE_URL}?query={movie_title}?'
         response = requests.get(API_URL, headers=HEADERS).json()
-        
         try:
             link = response.get('items')[0].get('link')
-            # print(link)
+            print(link)
+            subtitle = response.get('items')[0].get('subtitle')
+            print(subtitle)
+            actor = ', '.join(response.get('items')[0].get('actor')[:-1].split('|'))
+            print(actor)
             temp = link.split('=')
             naver_poster_code = temp[-1]
             # print(naver_poster_code)
-
             # 왕큰이미지를 띄우는 팝업링크
             thumb_url = BASE_IMAGE_URL + naver_poster_code
             hyp_link2 = requests.get(thumb_url)
@@ -71,12 +64,11 @@ with open('movie_naver.csv', 'w', encoding='utf-8', newline='') as f:
             soup2 = bs4.BeautifulSoup(html2, 'html.parser')
             poster_url = soup2.a.img['src']
             movie_dict['하이퍼텍스트_링크'] = link
+            movie_dict['영화명(영문)'] = subtitle
+            movie_dict['배우'] = actor
             # print(poster_url)
-
-
             movie_dict['썸네일_이미지의_URL'] = poster_url
             # pprint(movie_dict)
-            
             hyp_link = requests.get(link)
             # print(hyp_link.status_code)  /  200 -> 요청이 제대로 가짐
             # print('2', hyp_link)  /  200 -> 요청이 제대로 가짐
@@ -85,15 +77,12 @@ with open('movie_naver.csv', 'w', encoding='utf-8', newline='') as f:
             # print(soup)
             content = soup.select_one('div.story_area p.con_tx')
             movie_dict['줄거리'] = content.text
-
         except:
             pass
         # print('---------------')
-
         # movie_naver.csv 만들기
         writer.writerow(movie_dict)
         # pprint(movie_dict)
-
         # moive.db에 저장하는 부분
         @transaction.atomic
         def make_model():
@@ -119,6 +108,14 @@ with open('movie_naver.csv', 'w', encoding='utf-8', newline='') as f:
             except:
                 pass
             try:
+                movie.subtitle = movie_dict['영화명(영문)']
+            except:
+                pass
+            try:
+                movie.actor = movie_dict['배우']
+            except:
+                pass
+            try:
                 movie.movie_code = movie_dict['영화코드']
             except:
                 pass
@@ -130,11 +127,6 @@ with open('movie_naver.csv', 'w', encoding='utf-8', newline='') as f:
                 movie.description = movie_dict['줄거리']
             except:
                 pass
-            
             movie.save()
-
         if __name__ == "__main__":
             make_model()
-
-
-
--- movies/models.py
+++ movies/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 from django.conf import settings
-
-
 # MakeDB모델을 거쳐서 movie.csv -> 네이버API -> (Django MakeDB모델) -> DB
 class Movie(models.Model):
     # 필요한 정보 : rank, 국문 title, 영문 title, description, poster_url, naver_movie_url, -> csv에서부터 db를 만들기 위해 필요한 필드
@@ -11,17 +9,17 @@ class Movie(models.Model):
     end_date = models.IntegerField()
     rank = models.IntegerField()
     poster_url = models.CharField(max_length=200)
-    title = models.CharField(max_length=20)
-    movie_code = models.CharField(max_length=30)
-    naver_movie_url = models.CharField(max_length=200)
+    title = models.CharField(max_length=100)
+    subtitle = models.CharField(max_length=100)
+    actor = models.CharField(max_length=100)
+    movie_code = models.CharField(max_length=100)
+    naver_movie_url = models.CharField(max_length=100)
     description = models.TextField()
     clicked_users = models.ManyToManyField(  # movie.clicked_users.all()  =>  해당 영화를 클릭한 모든 유저를 불러오는 방법
         settings.AUTH_USER_MODEL,
         # user.clicked_movies.all()  =>  해당 유저가 클릭만 모든 영화를 불러오는 방법
         related_name='clicked_movies'
     )
-
-
 class Review(models.Model):
     content = models.CharField(max_length=50)
     score = models.IntegerField()
@@ -31,11 +29,8 @@ class Review(models.Model):
     # 영화 입장에서 나한테 달린 댓글들에 접근할 때 => movie.reviews.all()
     movie = models.ForeignKey(
         Movie, related_name='reviews', on_delete=models.CASCADE)
-
     class Meta:
         ordering = ('-pk', )
-
-
 class SearchedDate(models.Model):
     month = models.CharField(max_length=2)
     day = models.CharField(max_length=2)
