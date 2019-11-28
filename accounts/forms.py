ae555faabe36ef00fb3c04e01b4cf04f467edfc1
sungwoncho94 <sungwoncho94@naver.com>
2019-11-28 16:54:46 +0900

    폼을 변경해서 로그인 및 회원가입 양식 수정

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,9 +5,23 @@ from django import forms
 
 class CustomUserCreationForm(UserCreationForm):
 
+    username = forms.CharField(
+        label='Username',
+        max_length=254,
+    )
+    password1 = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput
+    )
+
+    password2 = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput
+    )
+
     class Meta:
         model = get_user_model()
-        fields = UserCreationForm.Meta.fields
+        fields = ['username', 'password1', 'password2']
 
 
 class CustomAuthenticationForm(AuthenticationForm):
